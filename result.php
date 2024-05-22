2006b1053009a279cb9394b92067626f35f4a59c
ariqirawann <165389522+ariqirawann@users.noreply.github.com>
2024-05-22 16:37:47 +0700

    result.php

--- result.php
+++ result.php
@@ -1,47 +1,59 @@
 <?php
 class CurrencyConverter {
-  private $currencies = array("USD", "EUR", "GBP");
+  private $currencies = array("IDR", "USD", "EUR", "GBP");
   private $exchangeRates = array(
-    "USD" => 0.000069,
-    "EUR" => 0.000061,
-    "GBP" => 0.000053
+    "USD" => 14000,
+    "EUR" => 15500,
+    "GBP" => 19000,
+    "IDR" => 1
   );
+
+  private $amount;
+  private $fromCurrency;
+  private $toCurrency;
   private $conversionHistory = array();
 
   public function __construct($amount, $fromCurrency, $toCurrency) {
-    $this->conversionHistory[] = array(
-      "amount" => $amount,
-      "fromCurrency" => $fromCurrency,
-      "toCurrency" => $toCurrency,
-      "result" => $this->convertCurrency($amount, $fromCurrency, $toCurrency)
-    );
-    if (count($this->conversionHistory) > 5) {
-      array_shift($this->conversionHistory);
+    $this->amount = $amount;
+    $this->fromCurrency = strtoupper($fromCurrency);
+    $this->toCurrency = strtoupper($toCurrency);
+  }
+
+  public function convertCurrency() {
+    if ($this->fromCurrency == "IDR") {
+      return $this->amount / $this->exchangeRates[$this->toCurrency];
+    } elseif ($this->toCurrency == "IDR") {
+      return $this->amount * $this->exchangeRates[$this->fromCurrency];
+    } else {
+      return ($this->amount / $this->exchangeRates[$this->fromCurrency]) * $this->exchangeRates[$this->toCurrency];
     }
   }
 
-  public function convertCurrency($amount, $fromCurrency, $toCurrency) {
-    if ($fromCurrency == "IDR") {
-      return $amount * $this->exchangeRates[$toCurrency];
-    } else {
-      return "Invalid from currency";
+  public function addToConversionHistory() {
+    $conversion = array(
+      'amount' => $this->amount,
+      'fromCurrency' => $this->fromCurrency,
+      'toCurrency' => $this->toCurrency,
+      'result' => $this->convertCurrency()
+    );
+    array_unshift($this->conversionHistory, $conversion);
+    if (count($this->conversionHistory) > 5) {
+      array_pop($this->conversionHistory);
     }
   }
 
   public function getConversionHistory() {
-    $history = "<table>";
-    $history.= "<thead><tr><th>Amount</th><th>From</th><th>To</th><th>Result</th></tr></thead>";
-    $history.= "<tbody>";
-    foreach (array_slice(array_reverse($this->conversionHistory), 0, 5) as $conversion) {
-      $history.= "<tr>";
-      $history.= "<td>". $conversion["amount"]. "</td>";
-      $history.= "<td>". $conversion["fromCurrency"]. "</td>";
-      $history.= "<td>". $conversion["toCurrency"]. "</td>";
-      $history.= "<td>". $conversion["result"]. " ". $conversion["toCurrency"]. "</td>";
-      $history.= "</tr>";
+    $history = '<table>';
+    $history .= '<tr><th>Amount</th><th>From</th><th>To</th><th>Result</th></tr>';
+    foreach ($this->conversionHistory as $conversion) {
+      $history .= '<tr>';
+      $history .= '<td>' . $conversion['amount'] . '</td>';
+      $history .= '<td>' . $conversion['fromCurrency'] . '</td>';
+      $history .= '<td>' . $conversion['toCurrency'] . '</td>';
+      $history .= '<td>' . number_format($conversion['result'], 2) . '</td>';
+      $history .= '</tr>';
     }
-    $history.= "</tbody>";
-    $history.= "</table>";
+    $history .= '</table>';
     return $history;
   }
 }
@@ -51,6 +63,8 @@ if (isset($_GET["amount"]) && isset($_GET["fromCurrency"]) && isset($_GET["toCur
   $fromCurrency = $_GET["fromCurrency"];
   $toCurrency = $_GET["toCurrency"];
   $currencyConverter = new CurrencyConverter($amount, $fromCurrency, $toCurrency);
+  $result = $currencyConverter->convertCurrency();
+  $currencyConverter->addToConversionHistory();
 ?>
     <html>
       <head>
@@ -60,10 +74,14 @@ if (isset($_GET["amount"]) && isset($_GET["fromCurrency"]) && isset($_GET["toCur
       <body>
         <div class="container">
           <h1>Currency Converter Result</h1>
-          <p>The result is: <?php echo $currencyConverter->convertCurrency($amount, $fromCurrency, $toCurrency);?> <?php echo $toCurrency;?></p>
-          <a href="index.php">Return to converter</a>
+          <p>The result is: <?php echo number_format($result, 2);?> <?php echo $toCurrency;?></p>
           <h2>Conversion History</h2>
           <?php echo $currencyConverter->getConversionHistory();?>
+          <div class="back-button">
+            <a href="index.php">
+              <div class="button">Return to Converter</div>
+            </a>
+          </div>
         </div>
       </body>
     </html>
